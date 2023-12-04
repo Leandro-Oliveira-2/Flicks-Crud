1ad9f03d0a119447b75a610a9f06e3438331f0d9
Leandro-Oliveira-2 <leandro.oliveira@dcx.ufpb.br>
2023-12-04 18:38:45 -0300

    project configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,15 +4,19 @@ module.exports = {
     node: true,
   },
   extends: [
-    "plugin:vue/essential",
-    "eslint:recommended",
-    "plugin:prettier/recommended",
+    'plugin:vue/essential',
+    'eslint:recommended',
+    'prettier', // Adiciona a configuração do Prettier
   ],
   parserOptions: {
-    parser: "@babel/eslint-parser",
+    parser: '@babel/eslint-parser',
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+  },
+  plugins: ['prettier'], // Adiciona o plugin do Prettier
+  rules: {
+    'prettier/prettier': 'error', // Adiciona a regra do Prettier
   },
 };
--- src/main.js
+++ src/main.js
@@ -1,12 +1,25 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import './App.scss'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { faFontAwesome } from '@fortawesome/free-brands-svg-icons'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+library.add(faCoffee, faFontAwesome)
+library.add(fas)
 
-Vue.config.productionTip = false;
-
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+Vue.config.productionTip = false
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 new Vue({
   router,
   store,
   render: (h) => h(App),
-}).$mount("#app");
+}).$mount('#app')
